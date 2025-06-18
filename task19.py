a6d4d4f0f2d01f145cbb51b55bd65a42aa9113a4
ubparviz <ubparviz@gmail.com>
2025-06-18 12:56:55 +0500

    Done

--- task19.py
+++ task19.py
@@ -1,6 +1,2 @@
-# Foydalanuvchidan matn oling. for yordamida nechta unli harf (a, e, i, o, u) borligini aniqlang.
 
-# Masalan: "hello world" → 3
-
-text = input("Matn kiriting: ")
 
--- task20.py
+++
@@ -1,2 +0,0 @@
-# Dastur foydalanuvchidan “O‘zbekiston poytaxti nima?” deb so‘raydi. 
-# “Toshkent” deb to‘g‘ri javob berguncha so‘rashda davom etadi.

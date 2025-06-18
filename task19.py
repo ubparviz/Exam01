6bc19415f359732017e329ec44946c2b15602f46
ubparviz <ubparviz@gmail.com>
2025-06-18 13:03:25 +0500

    Done

--- task19.py
+++ task19.py
@@ -1,2 +1,13 @@
+# Foydalanuvchidan matn oling. for yordamida nechta unli harf (a, e, i, o, u) borligini aniqlang.
 
+matn = input("Matn kiriting: ")
+
+unlilar = "aeiou"
+numbers_unlilar = 0
+
+for harf in matn.lower():
+    if harf in unlilar:
+        numbers_unlilar += 1
+
+print("Unli harflar soni:", numbers_unlilar)
 
